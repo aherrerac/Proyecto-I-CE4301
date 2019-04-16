d59497908c4c97e078fba594c1ac4c0c47fd9ef1
olbapd <pablod.garciab@gmail.com>
2019-04-15 22:18:14 -0600

    Branch complete.

--- Compiler/compiler.py
+++ Compiler/compiler.py
@@ -8,6 +8,11 @@ INSTRUCTIONS = ["add","addeq","addlt","addgt","addle",
                 "inv","iin","gtc","adr"]
 
 BRANCHES =["b","beq","blt","bgt","ble","bl","bleq","bllt","blgt","blle"]
+CONDITIONS_BRANCH={"eq":"00001010","lt":"10111010","gt":"11001010","le":"11011010",
+            "llt":"10111011","lgt":"11001011","lle":"11011011", "l":"00001011"}
+
+CONDITIONS={"eq":"0000","lt":"1011","gt":"1100","le":"1101",
+            "llt":"1011","lgt":"1100","lle":"1101", "l":"0000"}
 
 CURRENT_INSTR = 0
 
@@ -36,7 +41,7 @@ def read_file():
             compile_branch(linea[index]) #le indico cual es el label
             cont+=1
         elif CURRENT_INSTR == Instruction_Enum.Other:
-            compile_branch(linea[index]) #le indico cual es el label
+            compile_reg(linea[index:]) #le indico cual es el label
             cont+=1
         
     file.close()
@@ -82,26 +87,55 @@ def verify_instr(linea):
     else:
         print("Error 01: Instruction not found.")
 
-def compile_branch(label):
+def compile_branch(linea,current_add):
     found = False
     pos= 0
+    condition-"00001010"
     for lista in LABELS:
         if label in lista:
             found = True
             pos=lista[1]
             break
     if found:
-        a=1
-    	#La posicion a donde hacer el jump esta en pos	
+        for key, value in CONDITIONS_BRANCH.items():
+            if key in linea[0]:
+                condition=value
+                break
+        offset= current_add - LABELS.get(linea[1])
+        offset = "{:024b}".format(offset)
+        offset = "{:.24}".format(offset)
+        #se concatena el condition con el offset
     else:
     	print("Error 02: No se encontro el label: "+label)
 
 
-def compile_reg_imm():
-    a=1
+def compile_reg(lista):
+    reg1= lista[1]
+    if lista[2][0] == "r":
+    	reg2=lista[2]
+    else:
+      	num= num_to_bin(lista[2])
 
 def compile_reg_reg():
-    a=1     
+    a=1
+
+def num_to_bin(num):
+    if num[0]=='#':
+        if '0x' in num:
+            num="{0:b}".format(int(num[1:],16))
+        else:
+            num= "{0:b}".format(int(num[1:]))
+    elif num[0]=='=':
+        if '0x' in num:
+            num="{0:b}".format(int(num[1:],16))
+        else:
+            num= "{0:b}".format(int(num[1:]))
+    elif '0x' in num:
+    	num="{0:b}".format(int(num,16))
+    else:
+      	print("Error 03: Numero no valido.")
+    print (num)
+    
 
 class Instruction_Enum(Enum):
     Branch = 1
@@ -109,5 +143,5 @@ class Instruction_Enum(Enum):
     RegImm = 3
     RegReg = 4
     Label = 5
-
+#"{0:b}".format(int("0xF",16))
 read_file()
